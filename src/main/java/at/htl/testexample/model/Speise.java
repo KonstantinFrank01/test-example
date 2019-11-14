b0b68d3b750387c1371ddb123289e66b587a0712
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-11-14 17:24:40 +0100

    implemented speise model

--- src/main/java/at/htl/testexample/model/Speise.java
+++ src/main/java/at/htl/testexample/model/Speise.java
@@ -1,4 +1,43 @@
 package at.htl.testexample.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MY_SPEISE")
 public class Speise {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String speise;
+
+    public Speise() {
+    }
+
+    public Speise(String speise) {
+        this.speise = speise;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+
+    public String getSpeise() {
+        return speise;
+    }
+
+    public void setSpeise(String speise) {
+        this.speise = speise;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d: %s", id, speise);
+    }
 }
