f38efec300512018c492fdecd1b59e24291b21cd
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-11-14 17:23:41 +0100

    implemented wochentag model

--- src/main/java/at/htl/testexample/model/Wochentag.java
+++ src/main/java/at/htl/testexample/model/Wochentag.java
@@ -1,4 +1,36 @@
 package at.htl.testexample.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "MY_WOCHENTAG")
 public class Wochentag {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private
+    Long id;
+    private String tag;
+
+    public Wochentag() {
+    }
+
+    public Wochentag(String tag) {
+        this.tag = tag;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
