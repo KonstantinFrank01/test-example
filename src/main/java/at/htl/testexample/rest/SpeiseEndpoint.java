b12d128781f8efdc68607735faaa30cbb2ef376b
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-11-14 17:31:26 +0100

    implemented get-req to get all Speisen

--- src/main/java/at/htl/testexample/rest/SpeiseEndpoint.java
+++ src/main/java/at/htl/testexample/rest/SpeiseEndpoint.java
@@ -1,4 +1,25 @@
 package at.htl.testexample.rest;
 
+import at.htl.testexample.model.Speise;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("speise")
 public class SpeiseEndpoint {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Speise> getAlleSpeisen() {
+        return em.createNamedQuery("Speise.getAll", Speise.class).getResultList();
+    }
 }
+
